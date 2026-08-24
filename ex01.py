2c7aaf311b85b2df19a054d3fc0dd23ca226bdc0
Danilo Kofugi <dkofugi@gmail.com>
2026-08-24 19:27:14 -0300

    commit
    Refactor the program to take user input for addition instead of using hardcoded values.

--- ex01.py
+++ ex01.py
@@ -1,21 +1,11 @@
 # SOMA DE DOIS NUMEROS
 print("="*30)
-print("SOMA 1")
-x1= 4
-x2= 9
+print("SOMA")
+x1= int(input("Digite um numero"))
+x2= int(input("Digite um numero"))
 resultado= x1+x2
 print(f"{x1} + {x2} = {resultado}")
 print("="*30)
-print("SOMA 2")
 
-x1= -3
-x2= 8
-resultado= x1+x2
-print(f"{x1} + {x2} = {resultado}")
 
-print("="*30)
-print("SOMA 3")
-x1= 0
-x2= 12
-resultado= x1+x2
-print(f"{x1} + {x2} = {resultado}")
+
