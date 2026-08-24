db933f7c1738fceabac5c6b4c0eac80419b668f5
Danilo Kofugi <dkofugi@gmail.com>
2026-08-24 11:59:51 -0300

    primeiro commit

---
+++ ex06.py
@@ -0,0 +1,8 @@
+largura = float(input("Largura: "))
+altura = float(input("Altura: "))
+
+area = largura * altura
+perimetro = 2 * (largura + altura)
+
+print(f"\nÁrea: {area}")
+print(f"Perímetro: {perimetro}")
