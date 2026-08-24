c8c61430f19ce3cc9dc6db9c413df625d76497c8
dkofu <dkofugi@gmail.com>
2026-08-23 22:02:44 -0300

    primeiro commit

---
+++ ex01.py
@@ -0,0 +1,21 @@
+# SOMA DE DOIS NUMEROS
+print("="*30)
+print("SOMA 1")
+x1= 4
+x2= 9
+resultado= x1+x2
+print(f"{x1} + {x2} = {resultado}")
+print("="*30)
+print("SOMA 2")
+
+x1= -3
+x2= 8
+resultado= x1+x2
+print(f"{x1} + {x2} = {resultado}")
+
+print("="*30)
+print("SOMA 3")
+x1= 0
+x2= 12
+resultado= x1+x2
+print(f"{x1} + {x2} = {resultado}")
---
+++ ex02.py
@@ -0,0 +1,26 @@
+# MEDIA DE DUAS NOTAS
+
+x1= 5.5
+x2= 6
+resultado= (x1+x2)/2
+print(f"""NOTA1: {x1} 
+NOTA2: {x2}
+MÉDIA: {resultado}""")
+print("="*30)
+print("MÉDIA 2")
+x1= 10
+x2= 9
+resultado= (x1+x2)/2
+print(f"""NOTA1: {x1} 
+NOTA2: {x2}
+MÉDIA: {resultado}""")
+print("="*30)
+print("MÉDIA 3")
+x1= 0 
+x2= 4
+resultado= (x1+x2)/2
+print(f"""NOTA1: {x1} 
+NOTA2: {x2}
+MÉDIA: {resultado}""")
+
+
