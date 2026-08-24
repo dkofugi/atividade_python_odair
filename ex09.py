963b83f8eb42094e07ca2a8ffef49d32e643da40
Danilo Kofugi <dkofugi@gmail.com>
2026-08-24 12:01:02 -0300

    primeiro commit

---
+++ ex09.py
@@ -0,0 +1,7 @@
+salario_atual = float(input("Salário atual: "))
+
+aumento = salario_atual * 0.15
+novo_salario = salario_atual + aumento
+
+print(f"\nAumento: R$ {aumento:.2f}")
+print(f"Novo salário: R$ {novo_salario:.2f}")
