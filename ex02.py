214abe0c5e6f7c4d6ddb1be6de1f377a1c39a2bc
Danilo Kofugi <dkofugi@gmail.com>
2026-08-24 19:28:12 -0300

    commit
    Removed hardcoded values and added user input for grades.

--- ex02.py
+++ ex02.py
@@ -1,26 +1,12 @@
 # MEDIA DE DUAS NOTAS
 
-x1= 5.5
-x2= 6
+x1= float(input("Digite um numero"))
+x2= float(input("Digite um numero"))
 resultado= (x1+x2)/2
 print(f"""NOTA1: {x1} 
 NOTA2: {x2}
 MÉDIA: {resultado}""")
 print("="*30)
-print("MÉDIA 2")
-x1= 10
-x2= 9
-resultado= (x1+x2)/2
-print(f"""NOTA1: {x1} 
-NOTA2: {x2}
-MÉDIA: {resultado}""")
-print("="*30)
-print("MÉDIA 3")
-x1= 0 
-x2= 4
-resultado= (x1+x2)/2
-print(f"""NOTA1: {x1} 
-NOTA2: {x2}
-MÉDIA: {resultado}""")
+
 
 
