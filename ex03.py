b1aff2ab3a78a20a25cf965be4bf3b31199e9ce6
Danilo Kofugi <dkofugi@gmail.com>
2026-08-24 10:16:06 -0300

    Add program to calculate antecessor and sucessor

---
+++ ex03.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um número: "))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print(f"\nAntecessor: {antecessor}")
+print(f"Número: {numero}")
+print(f"Sucessor: {sucessor}")
